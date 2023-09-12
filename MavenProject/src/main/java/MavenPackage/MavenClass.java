1b12efe7d1e703c91f63e8738706f43467139570
febaz <febaz@Feba.bbrouter>
2023-09-12 07:46:54 +0530

    second commit

--- MavenProject/src/main/java/MavenPackage/MavenClass.java
+++ MavenProject/src/main/java/MavenPackage/MavenClass.java
@@ -5,6 +5,7 @@ public class MavenClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("My first maven project");
+		System.out.println("My first maven project");
 
 	}
 
